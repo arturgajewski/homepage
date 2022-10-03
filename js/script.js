83fa2f72007c54ec59832c69ea54b49bcc0db392
Artur Gajewski <arturgaj6@wp.pl>
2022-10-03 13:36:58 +0200

    add function to button

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-console.log("Witaj w devtools mojej strony :)");
-let body = document.querySelector(".body");
-let changeBackground = document.querySelector(".js-button__changeBackground");
-let darkBody = document.querySelector(".darkBody");
-let darkedName = document.querySelector(".darkedName");
-let changeText = document.innerText;
+function welcome() {
+  console.log("Witaj w devtools mojej strony :)");
+}
+welcome();
 
-changeBackground.addEventListener("click", () => {
+let onChangeBackgroundColor = () => 
+{
   body.classList.toggle("darkBody");
   if (body.classList.contains("darkBody")) {
     darkedName.innerText = "jasny";
   } else {
     darkedName.innerText = "ciemny";
   }
-});
+};
+
+let body = document.querySelector(".body");
+let changeBackground = document.querySelector(".js-button__changeBackground");
+let darkBody = document.querySelector(".darkBody");
+let darkedName = document.querySelector(".darkedName");
+let changeText = document.innerText;
+
+changeBackground.addEventListener("click", onChangeBackgroundColor);
